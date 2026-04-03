be4db221b1a37262bbe535c71ce55c025cf2da6c
신창용 <ckddyd0806@gmail.com>
2026-04-03 10:57:40 +0900

    Update pull-c4.sh

--- scripts/pull-c4.sh
+++ scripts/pull-c4.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-# Use HTTP: bash scripts/pull-c4.sh
-# Use GIT: USE_GIT=1 bash scripts/pull-c4.sh
+# Clone allenai/c4 under a chosen parent directory (creates .../c4).
+#
+# Use HTTP:
+#   bash scripts/pull-c4.sh
+#   bash scripts/pull-c4.sh /data/datasets
+#   TARGET=/data/datasets bash scripts/pull-c4.sh
+# Use GIT:
+#   USE_GIT=1 bash scripts/pull-c4.sh /data/datasets
+#
+# Parent directory: TARGET env overrides first argument; both default to current directory.
 set -e
+
+DEST="${TARGET:-${1:-.}}"
+mkdir -p "$DEST"
+cd "$DEST"
+echo "Destination parent: $(pwd) (repo will be ./c4)"
+
 # The git server seems to be faster than the http server
 # However, it requires ssh keys to be set up
 if [ "$USE_GIT" = "1" ]; then
